4532455a62b6097706e5e3a3172067dd66e49f64
Thiery Louison <bartender.mu@gmail.com>
2024-10-11 16:37:59 +0400

    Update telescope.lua file

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,70 +1,79 @@
+-- LazyVim configuration for telescope.nvim setup
 return {
-  'nvim-telescope/telescope.nvim',
-  dependencies = {
-    'nvim-lua/plenary.nvim',      -- Telescope dependency
-    {
-      'nvim-telescope/telescope-fzf-native.nvim',
-      build = 'make'  -- Compile the fzf-native extension
+  {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = {
+      {'nvim-lua/plenary.nvim'},
+      {'nvim-telescope/telescope-fzf-native.nvim', build = 'make'},
+      {'nvim-telescope/telescope-ui-select.nvim'},
+      {'nvim-telescope/telescope-file-browser.nvim'},
     },
+    config = function()
+      local telescope = require('telescope')
+      local actions = require('telescope.actions')
+
+      -- Telescope setup
+      telescope.setup({
+        defaults = {
+          prompt_prefix = "🔭 ",
+          selection_caret = "➜ ",
+          path_display = { "smart" },
+          sorting_strategy = "ascending",
+          layout_config = {
+            horizontal = {
+              preview_width = 0.6,
+              results_width = 0.8,
+            },
+            vertical = {
+              preview_height = 0.7,
+            },
+          },
+          mappings = {
+            i = {
+              ["<C-j>"] = actions.move_selection_next,
+              ["<C-k>"] = actions.move_selection_previous,
+              ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+              ["<C-c>"] = actions.close,
+            },
+          },
+        },
+        pickers = {
+          find_files = {
+            theme = "dropdown",
+            previewer = false,
+          },
+          live_grep = {
+            theme = "ivy",
+          },
+        },
+        extensions = {
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case",
+          },
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({
+              -- Add customization here if needed
+            })
+          },
+        },
+      })
+
+      -- Load Telescope extensions
+      telescope.load_extension('fzf')
+      telescope.load_extension('ui-select')
+      telescope.load_extension('file_browser')
+
+      -- Custom keybindings
+      vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope file_browser<CR>', { noremap = true, silent = true })
+    end,
   },
-  config = function()
-    local telescope = require('telescope')
-
-    telescope.setup{
-      defaults = {
-        vimgrep_arguments = {
-          'rg',
-          '--color=never',
-          '--no-heading',
-          '--with-filename',
-          '--line-number',
-          '--column',
-          '--smart-case',
-        },
-        prompt_prefix = '🔍 ',
-        selection_caret = '➤ ',
-        path_display = { 'smart' },
-        layout_strategy = 'horizontal',
-        layout_config = {
-          horizontal = {
-            prompt_position = 'top',
-            preview_width = 0.55,    -- Set the width of the preview window
-            results_width = 0.45,
-          },
-          width = 0.85,              -- Total width of the Telescope window
-          height = 0.80,             -- Total height of the Telescope window
-          preview_cutoff = 120,      -- When the preview should be hidden based on the window width
-        },
-        sorting_strategy = 'ascending',  -- Results appear from top to bottom
-        mappings = {
-          i = {
-            ['<C-n>'] = require('telescope.actions').move_selection_next,
-            ['<C-p>'] = require('telescope.actions').move_selection_previous,
-            ['<C-c>'] = require('telescope.actions').close,
-          },
-        },
-      },
-      pickers = {
-        find_files = {
-          theme = "dropdown",
-          previewer = true,         -- Enable file preview for find_files
-        },
-        buffers = {
-          theme = "dropdown",
-        },
-      },
-      extensions = {
-        fzf = {
-          fuzzy = true,                    -- false will only do exact matching
-          override_generic_sorter = true,  -- override the generic sorter
-          override_file_sorter = true,     -- override the file sorter
-          case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-        },
-      },
-    }
-
-    -- Load the fzf extension if installed
-    telescope.load_extension('fzf')
-  end,
 }
 
