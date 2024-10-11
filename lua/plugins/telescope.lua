b76428fc98dae676b7a6d0623e05aecf3e0d07d6
Thiery Louison <bartender.mu@gmail.com>
2024-10-11 08:01:03 +0400

    Update telescope configuration

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,25 +1,70 @@
 return {
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
+  'nvim-telescope/telescope.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',      -- Telescope dependency
+    { 
+      'nvim-telescope/telescope-fzf-native.nvim', 
+      build = 'make'  -- Compile the fzf-native extension
+    }, 
   },
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
+  config = function()
+    local telescope = require('telescope')
+
+    telescope.setup{
+      defaults = {
+        vimgrep_arguments = {
+          'rg',
+          '--color=never',
+          '--no-heading',
+          '--with-filename',
+          '--line-number',
+          '--column',
+          '--smart-case',
+        },
+        prompt_prefix = '🔍 ',
+        selection_caret = '➤ ',
+        path_display = { 'smart' },
+        layout_strategy = 'horizontal',
+        layout_config = {
+          horizontal = {
+            prompt_position = 'top',
+            preview_width = 0.55,    -- Set the width of the preview window
+            results_width = 0.45,
+          },
+          width = 0.85,              -- Total width of the Telescope window
+          height = 0.80,             -- Total height of the Telescope window
+          preview_cutoff = 120,      -- When the preview should be hidden based on the window width
+        },
+        sorting_strategy = 'ascending',  -- Results appear from top to bottom
+        mappings = {
+          i = {
+            ['<C-n>'] = require('telescope.actions').move_selection_next,
+            ['<C-p>'] = require('telescope.actions').move_selection_previous,
+            ['<C-c>'] = require('telescope.actions').close,
           },
         },
-      })
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>f", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
+      },
+      pickers = {
+        find_files = {
+          theme = "dropdown",
+          previewer = true,         -- Enable file preview for find_files
+        },
+        buffers = {
+          theme = "dropdown",
+        },
+      },
+      extensions = {
+        fzf = {
+          fuzzy = true,                    -- false will only do exact matching
+          override_generic_sorter = true,  -- override the generic sorter
+          override_file_sorter = true,     -- override the file sorter
+          case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+        },
+      },
+    }
 
-      require("telescope").load_extension("ui-select")
-    end,
-  },
+    -- Load the fzf extension if installed
+    telescope.load_extension('fzf')
+  end,
 }
+
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -16,3 +16,9 @@ vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.wo.number = true
 
+-- Key mappings for Telescope
+vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { desc = 'Find Files' })
+vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { desc = 'Live Grep' })
+vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { desc = 'Buffers' })
+vim.keymap.set('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', { desc = 'Help Tags' })
+
