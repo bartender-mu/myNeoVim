4c59277c71ccbec52c962fba0568e1f00e4932c8
Thiery Louison <bartender.mu@gmail.com>
2024-10-16 15:14:51 +0400

    remove swager preview

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,38 +1,38 @@
 return {
-    -- "folke/which-key.nvim",
-    -- config = function()
-    --     local wk = require("which-key")
-    --
-    --     wk.setup {
-    --         plugins = {
-    --             spelling = { enabled = true },
-    --             presets = {
-    --                 operators = true,
-    --                 motions = true,
-    --                 text_objects = true,
-    --                 windows = true,
-    --                 nav = true,
-    --                 z = true,
-    --                 g = true,
-    --             },
-    --         },
-    --         layout = {
-    --             height = { min = 4, max = 25 },
-    --             width = { min = 20, max = 50 },
-    --             spacing = 5,
-    --             align = "left",
-    --         },
-    --         show_help = true,
-    --         default_options = {
-    --             show_warnings = false, -- Disable warnings
-    --         },
-    --     }
-    --
-    --     -- Define your key mappings
-    --     wk.register({
-    --         -- { "<leader><leader>/", desc = "<cmd>CommentToggle<cr>" },
-    --         -- Other mappings here
-    --     }, {  })
-    -- end,
+    "folke/which-key.nvim",
+    config = function()
+        local wk = require("which-key")
+
+        wk.setup {
+            plugins = {
+                spelling = { enabled = true },
+                presets = {
+                    operators = true,
+                    motions = true,
+                    text_objects = true,
+                    windows = true,
+                    nav = true,
+                    z = true,
+                    g = true,
+                },
+            },
+            layout = {
+                height = { min = 4, max = 25 },
+                width = { min = 20, max = 50 },
+                spacing = 5,
+                align = "left",
+            },
+            show_help = true,
+            default_options = {
+                show_warnings = false, -- Disable warnings
+            },
+        }
+
+        -- Define your key mappings
+        wk.register({
+            -- { "<leader><leader>/", desc = "<cmd>CommentToggle<cr>" },
+            -- Other mappings here
+        }, {  })
+    end,
 }
 
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -7,20 +7,6 @@ vim.opt.swapfile = false
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.wo.number = true
 
-
-
--- -- Key mappings for LSP actions
--- vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
--- vim.api.nvim_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", { noremap = true, silent = true })
--- vim.api.nvim_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = true })
--- vim.api.nvim_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", { noremap = true, silent = true })
--- vim.api.nvim_set_keymap("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", { noremap = true, silent = true })
-
-
--- Key mappings for neo-tree
--- vim.api.nvim_set_keymap('n', '<leader>e', ':Neotree toggle<CR>', { noremap = true, silent = true })
-
-
 -- Define leader key
 vim.g.mapleader = " "  -- Space key as the leader key
 
@@ -46,32 +32,6 @@ map('n', '<leader>h', '<C-w>h', opts)            -- Move left between windows
 map('n', '<leader>j', '<C-w>j', opts)            -- Move down between windows
 map('n', '<leader>k', '<C-w>k', opts)            -- Move up between windows
 map('n', '<leader>l', '<C-w>l', opts)            -- Move right between windows
-
--- Plugin-specific mappings
--- Telescope (fuzzy finder)
--- map('n', '<leader>ff', ':Telescope find_files<CR>', opts)  -- Find files
--- map('n', '<leader>fg', ':Telescope live_grep<CR>', opts)   -- Live grep
--- map('n', '<leader>fb', ':Telescope buffers<CR>', opts)     -- List buffers
--- map('n', '<leader>fh', ':Telescope help_tags<CR>', opts)   -- Search help tags
-
--- -- LSP (Language Server Protocol)
--- map('n', '<leader>gd', ':lua vim.lsp.buf.definition()<CR>', opts)  -- Go to definition
--- map('n', '<leader>gr', ':lua vim.lsp.buf.references()<CR>', opts)  -- List references
--- map('n', '<leader>rn', ':lua vim.lsp.buf.rename()<CR>', opts)      -- Rename symbol
--- map('n', '<leader>ca', ':lua vim.lsp.buf.code_action()<CR>', opts) -- Show code actions
---
--- -- Debugging (DAP)
--- map('n', '<leader>db', ':lua require("dap").toggle_breakpoint()<CR>', opts) -- Toggle breakpoint
--- map('n', '<leader>dc', ':lua require("dap").continue()<CR>', opts)         -- Continue execution
--- map('n', '<leader>di', ':lua require("dap").step_into()<CR>', opts)        -- Step into
--- map('n', '<leader>do', ':lua require("dap").step_over()<CR>', opts)        -- Step over
--- map('n', '<leader>dr', ':lua require("dap").repl.open()<CR>', opts)        -- Open REPL
-
--- Git shortcuts (using 'tpope/vim-fugitive')
--- map('n', '<leader>gs', ':Git<CR>', opts)           -- Git status
--- map('n', '<leader>gc', ':Git commit<CR>', opts)    -- Git commit
--- map('n', '<leader>gp', ':Git push<CR>', opts)      -- Git push
--- map('n', '<leader>gl', ':Git log<CR>', opts)       -- Git log
 
 -- File operations (NvimTree)
 vim.api.nvim_set_keymap('n', '<leader>e', ':Neotree toggle<CR>', { noremap = true, silent = true })
